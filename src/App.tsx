b7a406420ff095f328e3d5339343f0dca616f6dd
André Martins <fmartins.andre@gmail.com>
2023-11-25 23:38:26 -0300

    chore: add libs to api management

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import "./global.css";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FormAgrotis } from "features/forms/form-agrotis";
 import { themeAgrotis } from "resources/theme/theme-agrotis";
 import { LayoutAgrotis } from "ui/layouts/layout-agrotis";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <ThemeProvider theme={themeAgrotis}>
-      <LocalizationProvider dateAdapter={AdapterLuxon}>
-        <CssBaseline />
-        <LayoutAgrotis>
-          <FormAgrotis />
-        </LayoutAgrotis>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline />
+          <LayoutAgrotis>
+            <FormAgrotis />
+          </LayoutAgrotis>
+        </QueryClientProvider>
       </LocalizationProvider>
     </ThemeProvider>
   );
