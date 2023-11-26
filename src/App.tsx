9bf38d297212b21527b677a1fa1986851f323c6c
André Martins <fmartins.andre@gmail.com>
2023-11-26 16:37:08 -0300

    chore: setup zod translation

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,22 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FormAgrotis } from "features/forms/form-agrotis";
+import i18next from "i18next";
 import { themeAgrotis } from "resources/theme/theme-agrotis";
 import { LayoutAgrotis } from "ui/layouts/layout-agrotis";
+import { z } from "zod";
+import { zodI18nMap } from "zod-i18n-map";
+import translation from "zod-i18n-map/locales/pt/zod.json";
 
 const queryClient = new QueryClient();
+
+i18next.init({
+  lng: "pt",
+  resources: {
+    pt: { zod: translation },
+  },
+});
+z.setErrorMap(zodI18nMap);
 
 function App() {
   return (
